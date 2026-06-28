763c6e8931d6b9f8923e220ca90305cc53ee0cd5
bsahil01 <bazadsahil10@gmail.com>
2026-06-28 23:35:25 +0530

    Update API URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:7002",
+    baseURL: "https://razorpay-work-production.up.railway.app",,
     withCredentials: true,
 });
 
